24ec5e83e6fa8a98f566d7d0742fec4d18421817
grzegorzdeveloper <72461057+grzegorzdeveloper@users.noreply.github.com>
2025-09-24 17:11:45 +0100

    initial commit

---
+++ assets/js/theme.js
@@ -0,0 +1,48 @@
+const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+const themeBtn = document.querySelector(".theme-btn");
+
+const logoImg = document.querySelector(".logo");
+const themeIcon = document.querySelector(".theme-btn-icon");
+
+let selectedTheme;
+
+function handleThemeChange(e) {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme) {
+    selectedTheme = savedTheme;
+  } else {
+    if (e.matches) {
+      selectedTheme = "dark";
+    } else {
+      selectedTheme = "light";
+    }
+  }
+
+  document.documentElement.setAttribute("data-theme", selectedTheme);
+
+  // Change images
+  if (selectedTheme === "light") {
+    logoImg.src = "assets/images/logo-light-theme.svg";
+    themeIcon.src = "assets/images/icon-moon.svg";
+  } else {
+    logoImg.src = "assets/images/logo-dark-theme.svg";
+    themeIcon.src = "assets/images/icon-sun.svg";
+  }
+}
+
+function toggleTheme() {
+  selectedTheme = selectedTheme === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", selectedTheme);
+
+  handleThemeChange();
+}
+
+mediaQuery.addEventListener("change", handleThemeChange);
+
+// Initial page load
+handleThemeChange(mediaQuery);
+
+themeBtn.addEventListener("click", toggleTheme);
